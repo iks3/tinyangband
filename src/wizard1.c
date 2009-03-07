65e2aa5fa11ce4dc62a90bf4fc7faec7f4ebe174
iks <iks@06637369-1e22-0410-b4fc-9df2d3465925>
2009-03-07 19:04:19 +0000

    英語版のartifact.spoで、採掘道具と矢を出力していなかったのを修正。
    git-svn-id: http://svn.sourceforge.jp/svnroot/xangband/TinyAngband/trunk@1730 06637369-1e22-0410-b4fc-9df2d3465925

--- src/wizard1.c
+++ src/wizard1.c
@@ -465,7 +465,9 @@ static grouper group_artifact[] =
 	{ TV_SWORD,             "Edged Weapons" },
 	{ TV_POLEARM,           "Polearms" },
 	{ TV_HAFTED,            "Hafted Weapons" },
+	{ TV_DIGGING,           "Shovels/Picks" },
 	{ TV_BOW,               "Bows" },
+	{ TV_ARROW,             "Ammo" },
 
 	{ TV_SOFT_ARMOR,        "Body Armor" },
 	{ TV_HARD_ARMOR,        NULL },
